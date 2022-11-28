0cb13d953ff2fc9accb0f0f3da837f891cfbd45a
Joanna Hao <j.hao3@share.epsb.ca>
2022-11-28 10:19:19 -0700

    Worked on large mammal population program.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # CSE2130-Project
 
 Large Mammal Populations Elk Island National Park
+
+## Pseudocode from Document
+Need to have
+- for growth calc 
+  - population year
+    - -> population year
+  - population north (+south)
+    - -> area of park
+    - -> fall population estimate 
+  - animal species being calculated
+    - -> species name
+
+Ideas about importing data
+- delete columns not needed 
--- main.py
+++ main.py
@@ -18,17 +18,23 @@ def getFileContent(filename) -> list:
     file = open(filename)
     text_list = file.readlines()
     file.close()
-    # clean up data
+    # --- clean up data
+    data_list = []
+    # already split by \n
     for i in range(len(text_list)):
+        # now working w a string
+
         # assuming not all rows have \n (often true for last row)
         if text_list[i][-1] == "\n":  # \n considered 1 char
-            text_list[i] = text_list[i][:-1]
+            text_list[i] = text_list[i][:-1]  # to make one long string
 
-        text_list[i] = text_list[i].split(",")
+        text_list[i].split(",")
 
         for j in range(len(text_list[i])):
-            if text_list[i][j].isnumeric():
+            if text_list[i][j].isnumeric():  # working w/ a string (list already split)
                 text_list[i][j] = int(text_list[i][j])
+            if text_list[i][j] == '"':
+                text_list[i][j] = "#"
 
     return text_list
 
@@ -44,7 +50,20 @@ Please choose an option:
 
 
 # --- Processing
+def setupAllSpecies(list_data):
+    global CURSOR, CONNECTION
 
+
+# def setupBison(list_data):
+#     global CURSOR, CONNECTION
+#
+#
+# def setupMoose(list_data):
+#     global CURSOR, CONNECTION
+#
+#
+# def setupDeer(list_data):
+#     global CURSOR, CONNECTION
 
 
 # --- Outputs
@@ -52,19 +71,22 @@ Please choose an option:
 
 
 # --- variables
-DATABASE_FILE = "large_mammals.db"
-FIRST_RUN = True
-if (pathlib.Path.cwd() / DATABASE_FILE).exists():
-    FIRST_RUN = False
-
-CONNECTION = sqlite3.connect(DATABASE_FILE)
-CURSOR = CONNECTION.cursor()
+# DATABASE_FILE = "large_mammals.db"
+# FIRST_RUN = True
+# if (pathlib.Path.cwd() / DATABASE_FILE).exists():
+#     FIRST_RUN = False
+#
+# CONNECTION = sqlite3.connect(DATABASE_FILE)
+# CURSOR = CONNECTION.cursor()
 
 if __name__ == "__main__":
     # ----- MAIN PROGRAM CODE ----- #
-    if FIRST_RUN:
-        # setup everything
+    #if FIRST_RUN:
+    # setup everything
+    CONTENT = getFileContent("Elk_Island_NP_Grassland_Forest_Ungulate_Population_1906-2017_data_reg.csv")
+    print(CONTENT)
 
+    """
     while True:
         # --- inputs
         CHOICE = menu()
@@ -92,3 +114,4 @@ if __name__ == "__main__":
         elif CHOICE == 3:
             print("Goodbye!")
             exit()
+        """
