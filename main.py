d8989ac3755ddb88e6e6f05dbd7c072f512b922a
Joanna Hao <j.hao3@share.epsb.ca>
2022-12-06 22:59:59 -0700

    Finished program.

--- main.py
+++ main.py
@@ -40,12 +40,11 @@ def getFileContent(filename) -> list:
             if text_list[i][j].isnumeric():
                 text_list[i][j] = int(text_list[i][j])
             elif text_list[i][j] == "NA":
-                text_list[i][j] = ""
+                text_list[i][j] = None
     return text_list
 
 
 def menu() -> int:
-    # print("Welcome to the Elk Island National Park Large Mammal population database! ")
     print("""
 Please choose an option:
 1. Search Population Growth
@@ -73,82 +72,118 @@ def getNewYearData():
         For questions without the required (R) symbol, leave the field blank if there's no data.
         """)
         park_area = input("Area of park? (R) ")
-        if park_area not in ("North", "South"):
+        if park_area == "" or park_area.isnumeric() or park_area.capitalize() not in ("North", "South"):
             print("Field cannot be left blank. Please only enter the appropriate text. (North/South)")
             continue
         population_year = input("Population year? (R) ")
         if population_year == "" or not population_year.isnumeric():
-            print("Field cannot be left blank. Please only enter numbers.")
+            print("Field cannot be left blank. Please enter a valid number for this field.")
             continue
         survey_year = input("Survey year? ")
+        if survey_year == "":
+            pass
+        elif not survey_year.isnumeric():
+            print("Please only enter numbers for this field.")
+            continue
         survey_month = input("Survey month? (1-12) ")
+        if survey_month == "":
+            pass
+        elif not survey_month.isnumeric() or not 1 <= int(survey_month) <= 12:
+            print("Please enter a valid number for this field.")
+            continue
         survey_day = input("Survey day? (1-31) ")
+        if survey_day == "":
+            pass
+        elif not survey_day.isnumeric() or not 1 <= int(survey_day) <= 31:
+            print("Please enter a valid number for this field.")
+            continue
         species = input("Species name? (R) ")
         if species == "" or species.isnumeric():
             print("Field cannot be left blank. Please only enter the appropriate text.")
             continue
         species = species.capitalize()
         unknown_age_sex_count = input("Number of animals with unknown age and sex? ")
-        adult_male = input("Number of adult males? ")
-        adult_female = input("Number of adult females? ")
-        unknown_adult_count = input("Number of adults of unknown sex? ")
-        yearling_count = input("Number of yearlings? ")
-        calf_count = input("Number of calves? ")
-        survey_total = input("Survey total? ")
-        sightability_correction_factor = input("Sightability correction factor? ")
-        extra_captives = input("Number of additional captives? ")
-        animals_removed = input("Number of animals removed prior to survey? ")
-        fall_population = input("Estimate of fall population? (R) ")
-        if fall_population == "" or not fall_population.isnumeric():
-            print("Field cannot be left blank. Please only enter numbers.")
+        if unknown_age_sex_count == "":
+            pass
+        elif not unknown_age_sex_count.isnumeric():
+            print("Please enter a valid number for this field.")
             continue
-        comment = input("Survey comment: ")  # need to put quotes around if ',' inside
-        if "," in comment:
-            comment = '"' + comment + '"'
+        adult_male = input("Number of adult males? ")
+        if adult_male == "":
+            pass
+        elif not adult_male.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        adult_female = input("Number of adult females? ")
+        if adult_female == "":
+            pass
+        elif not adult_female.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        unknown_adult_count = input("Number of adults of unknown sex? ")
+        if unknown_adult_count == "":
+            pass
+        elif not unknown_adult_count.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        yearling_count = input("Number of yearlings? ")
+        if yearling_count == "":
+            pass
+        elif not yearling_count.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        calf_count = input("Number of calves? ")
+        if calf_count == "":
+            pass
+        elif not calf_count.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        survey_total = input("Survey total? ")
+        if survey_total == "":
+            pass
+        elif not survey_total.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        sightability_correction_factor = input("Sightability correction factor? ")
+        if sightability_correction_factor == "":
+            pass
+        elif not sightability_correction_factor.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        extra_captives = input("Number of additional captives? ")
+        if extra_captives == "":
+            pass
+        elif not extra_captives.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        animals_removed = input("Number of animals removed prior to survey? ")
+        if animals_removed == "":
+            pass
+        elif not animals_removed.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        fall_population = input("Estimate of fall population? ")
+        if not fall_population.isnumeric():
+            print("Please enter a valid number for this field.")
+            continue
+        if not fall_population.isnumeric():
+            print("Please only enter numbers.")
+            continue
+        comment = input("Survey comment: ")
         method = input("Estimate method? ")
         break
 
     new_data = [park_area, population_year, survey_year, survey_month, survey_day, species, unknown_age_sex_count, adult_male, adult_female, unknown_adult_count, yearling_count, calf_count, survey_total, sightability_correction_factor, extra_captives, animals_removed, fall_population, comment, method]
-
-    # for i in range(len(new_data)):
-    #     if new_data[i] == "" or new_data[i] == None:
-    #         new_data[i] = 0
 
     for i in range(len(new_data)):
         if new_data[i] == "":
             new_data[i] = None
         elif new_data[i].isnumeric():
             new_data[i] = int(new_data[i])
-    # ['North', 2018, None, None, None, 'Elk', None, None, None, None, None, None, None, None, None, None, 100, 'asdf', 'Aerial']
-    print(len(new_data))
-    global CURSOR, CONNECTION
-    for i in range(len(new_data)):
-        print(new_data[i])
-        CURSOR.execute("""
-                INSERT INTO
-                    populations
-                VALUES (
-                    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
-                )
-            ;""", new_data)
-
-    CONNECTION.commit()
-
+    return new_data
 
 
 # --- Processing
-def dataValidationSurveyDate(variable, lower_bound, upper_bound):
-    if variable == "":
-        return variable
-    elif variable.isnumeric():
-        variable = int(variable)
-        if variable < lower_bound or variable > upper_bound:
-            print(f"Please enter a valid number.")
-            return getNewYearData()
-    else:
-        print("Please only enter numbers.")
-
-
 def setupContent(data_list):
     global CURSOR, CONNECTION
     CURSOR.execute("""
@@ -170,7 +205,7 @@ def setupContent(data_list):
                 sightability_correction_factor INTEGER,
                 additional_captive_count INTEGER,
                 animals_removed_before_survey INTEGER,
-                fall_population_estimate INTEGER NOT NULL,
+                fall_population_estimate INTEGER,
                 survey_comment TEXT,
                 estimate_method TEXT
             )
@@ -188,18 +223,15 @@ def setupContent(data_list):
     CONNECTION.commit()
 
 
-def insertData(list_data):
+def insertNewData(list_data):
     global CURSOR, CONNECTION
-    for i in range(len(list_data)):
-        print(list_data[i])
-        CURSOR.execute("""
-            INSERT INTO
-                populations
-            VALUES (
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
-            )
-        ;""", list_data[i])
-
+    CURSOR.execute("""
+        INSERT INTO
+            populations
+        VALUES (
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+        )
+    ;""", list_data)
     CONNECTION.commit()
 
 
@@ -236,8 +268,6 @@ def getPopulationsData(year):
             population_year = ?
     ;""", [year]).fetchall()
 
-    print(year_data)
-
     year_total = 0
     for i in range(len(year_data)):
         for j in range(len(year_data[i])):
@@ -267,7 +297,7 @@ if __name__ == "__main__":
         setupContent(CONTENT)
 
     while True:
-        print("Welcome to the Elk Island National Park Large Mammal population database! ")  # potential duplicate that could be in menu
+        print("Welcome to the Elk Island National Park Large Mammal population database! ")
         # --- inputs
         CHOICE = menu()
         if CHOICE == 1:
@@ -283,6 +313,8 @@ if __name__ == "__main__":
                 POPULATION_CHANGE = START_POPULATION - END_POPULATION
                 TIME_CHANGE = START_YEAR - END_YEAR
                 GROWTH = POPULATION_CHANGE/TIME_CHANGE
+                if int(GROWTH) == GROWTH:
+                    GROWTH = int(GROWTH)
             else:
                 SPECIES = SPECIES_OPTIONS[SPECIES]
                 START_POPULATION = getSpeciesPopulationData(START_YEAR, SPECIES)
@@ -293,9 +325,7 @@ if __name__ == "__main__":
                 if int(GROWTH) == GROWTH:
                     GROWTH = int(GROWTH)
         elif CHOICE == 2:
-            # print(NEW_DATA)
-            # insertData(NEW_DATA)
-            pass
+            insertNewData(NEW_DATA)
 
         # --- outputs
         if CHOICE == 1:
@@ -305,3 +335,5 @@ if __name__ == "__main__":
         elif CHOICE == 3:
             print("Goodbye!")
             exit()
+        else:
+            print("Please enter a valid option number")
